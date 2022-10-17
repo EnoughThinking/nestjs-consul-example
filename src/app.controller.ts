b96729e642b0ed8770547227be42f49e74cbd7bf
EnoughThinking <justinsetonpro1994@gmail.com>
2022-10-17 18:22:04 +0400

    feat: Add NestjsConsulKvRealtimeModule to the application

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConsulKeyValue } from 'nestjs-consul-kv-realtime';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  @ConsulKeyValue({
+    key: 'env',
+  })
+  consulEnvironments!: { HELLO_MESSAGE: string };
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('consul')
+  getConsulHello(): string {
+    return this.consulEnvironments.HELLO_MESSAGE;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { NestjsConsulKvRealtimeModule } from 'nestjs-consul-kv-realtime';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    NestjsConsulKvRealtimeModule.forRootAsync({
+      useFactory: async () => ({
+        port: '8500',
+        host: 'localhost',
+        defaults: {
+          token: process.env.CONSUL_HTTP_TOKEN,
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
